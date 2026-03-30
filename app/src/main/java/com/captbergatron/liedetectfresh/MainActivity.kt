e2ab9f888e66560e2fc1e70a4159c6128ac5ae68
deckerloyd <deckerloyd@gmail.com>
2026-03-30 00:41:42 -0400

    L02: 1. separation of concepts 2. fingerprint box added (fixed libs) 3. scanning animation next

--- app/src/main/java/com/captbergatron/liedetectfresh/MainActivity.kt
+++ app/src/main/java/com/captbergatron/liedetectfresh/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
